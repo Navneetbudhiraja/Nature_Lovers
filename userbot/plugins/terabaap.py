2d14f85d56c9b078b9ad2a6fc7d3b96abe1c27a3
Navneetbudhiraja <68393316+Navneetbudhiraja@users.noreply.github.com>
2020-08-26 10:18:51 +0530

    Create terabaap.py

---
+++ userbot/plugins/terabaap.py
@@ -0,0 +1,33 @@
+"""Fun pligon...for HardcoreUserbot
+\nCode by @Kraken_The_BadASS
+type .anonymous to see the fun.
+"""
+import random, re
+from uniborg.util import admin_cmd
+import asyncio
+from telethon import events
+
+@borg.on(admin_cmd(pattern="anonymous ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("SUN BE馃槒")
+        await asyncio.sleep(1.5)
+        await event.edit("JANTA BHI HAI馃KISSE BAAT KRHA HAI馃檮")
+        await asyncio.sleep(2)
+        await event.edit("APNE BAAP SE馃槇")
+        await asyncio.sleep(1.8)
+        await event.edit("AUR馃槑JANNA CHAEE GA APNE馃槇BAAP KA NAAM")
+        await asyncio.sleep(2)
+        await event.edit("RUDRA馃槇")
+        await asyncio.sleep(2)
+        await event.edit("SUNA KI NHI馃槀")
+        await asyncio.sleep(1.6) 
+        await event.edit("Rudra馃槇TERA BAAP馃槑")
+        await asyncio.sleep(1.8)
+        await event.edit("YAKIN馃檮 NHI HOTA馃槒")
+        await asyncio.sleep(1.9)
+        await event.edit("TO JAKE馃槀 PUCH LE 馃槇 APNI MAA SE馃槑")
+        await asyncio.sleep(2)
+        await event.edit("Rudra Sir")
+        await asyncio.sleep(1.2)
+        await event.edit("BAAP")
