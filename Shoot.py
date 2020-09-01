ff773b9b8b27cedf62814b5917784f494ebc2509
Navneetbudhiraja <68393316+Navneetbudhiraja@users.noreply.github.com>
2020-09-01 13:42:15 +0530

    Create Shoot.py

---
+++ Shoot.py
@@ -0,0 +1,8 @@
+from userbot.utils import admin_cmd 
+
+@borg.on(admin_cmd(pattern = "shoot$"))
+async def _(cat):
+    await cat.edit("_/﹋\_\
+                           \n(҂`_´)\
+                           \n<,︻╦╤─ ҉ - -\
+                           \n_/﹋\_")
